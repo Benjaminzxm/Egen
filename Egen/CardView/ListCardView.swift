00929f9e4ecaadb67068035b686e43120fd3b156
YinjianChen <YinjianChen02@gmail.com>
2018-03-17 23:49:39 +0800

    delete some file

---
+++ Egen/CardView/ListCardView.swift
@@ -0,0 +1,21 @@
+//
+//  ListCardView.swift
+//  Egen
+//
+//  Created by YinjianChen on 2018/3/14.
+//  Copyright © 2018年 YinTokey. All rights reserved.
+//
+
+import UIKit
+
+class ListCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Egen/CardView/NormalView.swift
@@ -0,0 +1,24 @@
+//
+//  NormalView.swift
+//  Egen
+//
+//  Created by YinjianChen on 2018/3/14.
+//  Copyright © 2018年 YinTokey. All rights reserved.
+//
+
+import UIKit
+
+class NormalView: UIView {
+
+    var icon:UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
