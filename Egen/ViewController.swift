5b22305c84a3015b0dab172bdebf5965d198ce8d
YinjianChen <YinjianChen02@gmail.com>
2018-04-06 18:13:42 +0800

    test publish subject

--- Egen/ViewController.swift
+++ Egen/ViewController.swift
@@ -10,33 +10,59 @@ import UIKit
 import RxSwift
 import RxCocoa
 class ViewController: UIViewController {
-
+    var tableView = UITableView.init(frame: CGRect(x: 0, y: 0, width:414, height: 718))
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        
 
-//        let eventNum = Observable.from(["1","2","3","4","5","6","7"]).map { Int($0)}
-//            .filter {
-//                if let item = $0, item % 2 == 0{
-//                    print("event \(item)")
-//                    return true
-//                }
-//                return false
-//        }
-//
-//        eventNum.subscribe(
-//            onNext:{event in print("onNext \(String(describing: event))")},
-//            onError:{ print($0)},
-//            onCompleted:{print("complete")}
-//        )
-//
-//        let toggleSwitch = UISwitch.init(frame: CGRect(x: 20, y: 20, width: 40, height: 30))
-//        view.addSubview(toggleSwitch)
-//        toggleSwitch.rx.isOn
-//            .subscribe({ enable in
-//                print(enable)
-//            })
     }
 
 
+    func test1(){
+        //        let eventNum = Observable.from(["1","2","3","4","5","6","7"]).map { Int($0)}
+        //            .filter {
+        //                if let item = $0, item % 2 == 0{
+        //                    print("event \(item)")
+        //                    return true
+        //                }
+        //                return false
+        //        }
+        //
+        //        eventNum.subscribe(
+        //            onNext:{event in print("onNext \(String(describing: event))")},
+        //            onError:{ print($0)},
+        //            onCompleted:{print("complete")}
+        //        )
+        //
+        //        let toggleSwitch = UISwitch.init(frame: CGRect(x: 20, y: 20, width: 40, height: 30))
+        //        view.addSubview(toggleSwitch)
+        //        toggleSwitch.rx.isOn
+        //            .subscribe({ enable in
+        //                print(enable)
+        //            })}
+    
+    }
+    
+    func testPublishSubject(){
+        let disposeBag = DisposeBag()
+        let publishSubject = PublishSubject<String>()
+        publishSubject.subscribe{ e in
+            print("subscription 1, event: \(e)")
+            
+            }.disposed(by: disposeBag)
+        
+        publishSubject.onNext("a")
+        publishSubject.onNext("b")
+        
+        publishSubject.subscribe{ e in
+            print("subscription 2, event \(e)")
+            
+            }.disposed(by: disposeBag)
+        
+        publishSubject.onNext("c")
+        publishSubject.onNext("d")
+    }
 }
 
