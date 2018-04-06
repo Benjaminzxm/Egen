d8a63086f8139cdb7642f811570638b71404af88
YinjianChen <YinjianChen02@gmail.com>
2018-04-06 10:36:42 +0800

    UISwitch test

--- Egen/ViewController.swift
+++ Egen/ViewController.swift
@@ -8,27 +8,35 @@
 
 import UIKit
 import RxSwift
-
+import RxCocoa
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-       var eventNum = Observable.from(["1","2","3","4","5","6","7"]).map { Int($0)}
-            .filter {
-                if let item = $0, item % 2 == 0{
-                    print("event \(item)")
-                    return true
-                }
-                return false
-        }
-        
-        eventNum.subscribe(
-            onNext:{event in print("onNext \(String(describing: event))")},
-            onError:{ print($0)},
-            onCompleted:{print("complete")}
-        )
+
+//        let eventNum = Observable.from(["1","2","3","4","5","6","7"]).map { Int($0)}
+//            .filter {
+//                if let item = $0, item % 2 == 0{
+//                    print("event \(item)")
+//                    return true
+//                }
+//                return false
+//        }
+//
+//        eventNum.subscribe(
+//            onNext:{event in print("onNext \(String(describing: event))")},
+//            onError:{ print($0)},
+//            onCompleted:{print("complete")}
+//        )
+//
+//        let toggleSwitch = UISwitch.init(frame: CGRect(x: 20, y: 20, width: 40, height: 30))
+//        view.addSubview(toggleSwitch)
+//        toggleSwitch.rx.isOn
+//            .subscribe({ enable in
+//                print(enable)
+//            })
     }
+
 
 }
 
