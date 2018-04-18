9aa6076931a45a02a97276f2466158e4a0e683bb
YinjianChen <YinjianChen02@gmail.com>
2018-04-19 07:28:43 +0800

    remvoe rx

--- Egen/ViewController.swift
+++ Egen/ViewController.swift
@@ -7,79 +7,18 @@
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
+
 class ViewController: UIViewController {
-    var tableView = UITableView.init(frame: CGRect(x: 0, y: 0, width:414, height: 718))
-    let disposeBag = DisposeBag()
+    public var tableView = UITableView.init(frame: CGRect(x: 0, y: 0, width:414, height: 718))
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(tableView)
         
        
     }
 
 
-    func test1(){
-        //        let eventNum = Observable.from(["1","2","3","4","5","6","7"]).map { Int($0)}
-        //            .filter {
-        //                if let item = $0, item % 2 == 0{
-        //                    print("event \(item)")
-        //                    return true
-        //                }
-        //                return false
-        //        }
-        //
-        //        eventNum.subscribe(
-        //            onNext:{event in print("onNext \(String(describing: event))")},
-        //            onError:{ print($0)},
-        //            onCompleted:{print("complete")}
-        //        )
-        //
-        //        let toggleSwitch = UISwitch.init(frame: CGRect(x: 20, y: 20, width: 40, height: 30))
-        //        view.addSubview(toggleSwitch)
-        //        toggleSwitch.rx.isOn
-        //            .subscribe({ enable in
-        //                print(enable)
-        //            })}
-    
-    }
-    
-    func testPublishSubject(){
-        let publishSubject = PublishSubject<String>()
-        publishSubject.subscribe{ e in
-            print("subscription 1, event: \(e)")
-            
-            }.disposed(by: disposeBag)
-        
-        publishSubject.on(.next("a"))
-        publishSubject.onNext("b")
-        
-        publishSubject.subscribe{ e in
-            print("subscription 2, event \(e)")
-            
-            }.disposed(by: disposeBag)
-        
-        publishSubject.onNext("c")
-        publishSubject.onNext("d")
-    }
-    
-    func testBehaviorSubject(){
-        let behaviorSubject = BehaviorSubject.init(value: "z")
-        behaviorSubject.subscribe{ e in
-            print("sub1 ,event : \(e)")
-        }
-        
-    }
-    
-    func testMap(){
-        let originalSequence = Observable.of(1,2,3)
-        originalSequence
-            .map{ $0 * 5}
-            .subscribe(onNext:{print($0)})
-            .disposed(by: disposeBag)
-    }
 }
 
